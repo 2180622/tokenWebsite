f5594d0789ae882bc66b2ba9a1c6cc5172110e3c
2180622 <2180622@my.ipleiria.pt>
2021-04-30 13:15:38 +0100

    pretty urls (.htaccess)

--- backend/config/main.php
+++ backend/config/main.php
@@ -42,7 +42,6 @@ return [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
-                'class' => 'yii\rest\UrlRule',
             ],
         ],
     ],
